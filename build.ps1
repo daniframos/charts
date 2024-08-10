81331f52e77e8d658ac65886b616a31efd527ef0
Daniel Ramos <daniel.framos@cassi.com.br>
2024-08-10 18:36:26 -0300

    feito todos os charts

---
+++ build.ps1
@@ -0,0 +1,6 @@
+helm package .\grafana\ --version 0.0.1
+helm package .\nexus\ --version 0.0.1
+helm package .\openshift-proxmox-csi-plugin\ --version 0.0.1
+helm package .\prometheus\ --version 0.0.1
+
+helm repo index --merge index.yaml .
